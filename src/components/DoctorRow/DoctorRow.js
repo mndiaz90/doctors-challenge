1a457b4ad0cbda5d1bdcc9ec3ecf0e79ca9f044b
mndiaz90 <mayler900123@gmail.com>
2021-04-03 12:11:38 -0300

    Fix code style

--- src/components/DoctorRow/DoctorRow.js
+++ src/components/DoctorRow/DoctorRow.js
@@ -13,16 +13,20 @@ const DoctorRow = (props) => {
     }
 
     return <>
-        <tr data-upin={props.doctor.upin} className="rowDoctor" 
-            style={props.doctor.available ? {} : {textDecoration: "line-through", fontStyle: "italic", background: "rgb(229 234 236)"
-        }}
+        <tr data-upin={props.doctor.upin}
+            className="rowDoctor"
+            style={props.doctor.available ? {} : {
+                textDecoration: "line-through", fontStyle: "italic", background: "rgb(229 234 236)"
+            }}
         >
             <td>{props.doctor.upin}</td>
             <td>{props.doctor.name}</td>
             <td>{props.doctor.zipcode}</td>
             <td>{props.doctor.city}</td>
             <td>
-                <button className={props.doctor.available ? "button_unavailable" : "button_available"} onClick={() => onClickButton(props.doctor)}>
+                <button
+                    className={props.doctor.available ? "button_unavailable" : "button_available"}
+                    onClick={() => onClickButton(props.doctor)}>
                     {props.doctor.available ? 'Mark as Unavailable' : 'Mark as Available'}
                 </button>
             </td>
