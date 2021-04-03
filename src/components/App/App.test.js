e6bb70ab242c1adc4a04151a0837a028d7387c68
mndiaz90 <mayler900123@gmail.com>
2021-04-03 19:43:52 -0300

    Add dependecies to mock different fetch calls
    -Add scenario to test change the avaliability of doctors

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,6 +5,7 @@ import { act } from "react-dom/test-utils";
 import doctorsTable from "../../../tests/DataMock/DoctorsTable"
 import doctorsResponse from "../../../tests/DataMock/DoctorsResponse"
 import App from "./App";
+import config from "../../../config"
 
 let container = null;
 
@@ -12,11 +13,11 @@ beforeEach(async () => {
   container = document.createElement("div");
   document.body.appendChild(container);
 
-  global.fetch = jest.fn().mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(doctorsResponse)
-    })
-  );
+  fetch.mockResponse(req =>
+    req.url === `${config.serverEndpoint}/doctors`
+      ? Promise.resolve(JSON.stringify(doctorsResponse))
+      : Promise.resolve(JSON.stringify({ "upin": 202029, "name": "John Doe", "available": true }))
+  )
 
   await act(async () => {
     render(<App doctorsTable={doctorsTable} />, container);
@@ -24,11 +25,6 @@ beforeEach(async () => {
 });
 
 afterEach(() => {
-  if (global.fetch != undefined) {
-    global.fetch.mockClear();
-    delete global.fetch;
-  }
-
   unmountComponentAtNode(container);
   container.remove();
   container = null;
@@ -101,4 +97,20 @@ it("filters by UPIN", () => {
   expect(doctorNameTd.textContent).toBe("Kevin Lamkin");
   expect(doctorZipCodeTd.textContent).toContain("92015");
   expect(doctorCityTd.textContent).toContain("San Diego");
+});
+
+it("updates a doctor availability", async () => {
+  const doctorTr = container.querySelector("[data-upin='202029']");
+  const lastTd = doctorTr.querySelector(":last-child")
+  const toggleButton = lastTd.querySelector(":first-child")
+
+  expect(toggleButton.classList.contains('button_available')).toBe(true)
+  expect(toggleButton.classList.contains('button_unavailable')).toBe(false)
+
+  await act(async () => {
+    fireEvent.click(toggleButton)
+  });
+
+  expect(toggleButton.classList.contains('button_unavailable')).toBe(true)
+  expect(toggleButton.classList.contains('button_available')).toBe(false)
 });
