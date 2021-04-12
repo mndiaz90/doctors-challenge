dea71e189cee9be5f6a074daa4555d6ae8b9e02a
mndiaz90 <mayler900123@gmail.com>
2021-04-12 13:36:14 -0300

    Change loop `for` for `map`

--- src/index.util.js
+++ src/index.util.js
@@ -1,16 +1,15 @@
 function getDoctorsTable() {
     const doctorRows = document.getElementsByTagName("tbody")[0].rows;
-    let doctorsTable = [];
-
-    for (var i = 0; i < doctorRows.length; i++) {
-        var td = doctorRows[i].getElementsByTagName("td");
-        doctorsTable.push({
-            upin: doctorRows[i].getAttribute('data-upin'),
+    const arrayDoctors = Array.from(doctorRows);
+    const doctorsTable = arrayDoctors.map((row) => {
+        const td = row.getElementsByTagName("td");
+        return {
+            upin: row.getAttribute('data-upin'),
             name: td[0].innerText,
             zipcode: td[1].innerText,
             city: td[2].innerText,
-        });
-    }
+        }
+    });
 
     return doctorsTable;
 }
